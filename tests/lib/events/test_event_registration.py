b568964a4f24be2b8b14ca3994d1c4c297816384
Emilio Arenas <arenas.emilio@gmail.com>
2020-12-25 18:20:11 +0100

    Test events can be dataclasses

--- tests/lib/events/test_event_registration.py
+++ tests/lib/events/test_event_registration.py
@@ -1,4 +1,5 @@
 import pytest
+from dataclasses import dataclass
 
 from lib.events.event import Event
 from lib.events.event import EventType
@@ -24,5 +25,22 @@ def test_name_collision_raises_exception(clear_events):
         NAME = "MyEvent"
 
     with pytest.raises(RuntimeError):
+
         class OtherEvent(Event):
             NAME = "MyEvent"
+
+
+def test_event_dataclasses(clear_events):
+    """
+    Events can be dataclasses
+    """
+
+    @dataclass
+    class UserCreatedEvent(Event):
+        NAME = "user-created"
+        id: str
+
+    event = UserCreatedEvent(id="1234")
+
+    assert event.NAME == UserCreatedEvent.NAME
+    assert event.id == "1234"
